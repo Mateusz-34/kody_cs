c9b8ccdd6e53302591679dcb98c5265c96de2bb6
Mateusz Ostrozka <mateusz.ostrozka34@gmail.com>
2026-03-12 17:46:50 +0100

    Dodanie klasy Laptop

--- Product.cs
+++ Product.cs
@@ -46,14 +46,37 @@ public class Telewizor : Produkt
     }
 }
 
+public class Laptop : Produkt
+{
+    public int RAM;
+    public string Procesor;
+
+    public Laptop(string nazwa, double cena, string producent, int RAM, string Procesor) : base(nazwa, cena, producent)
+    {
+        this.RAM = RAM;
+        this.Procesor = Procesor;
+    }
+
+    public override void WyswietlInformacje()
+    {
+        Console.WriteLine("=== PRODUKT RTV ===");
+        Console.WriteLine(nazwa);
+        Console.WriteLine(producent);
+        Console.WriteLine(cena);
+        Console.WriteLine(RAM);
+        Console.WriteLine(Procesor);
+        Console.WriteLine(" ");
+    }
+}
+
 class Program
 {
     static void Main()
     {
         Telewizor Tv1 = new Telewizor("Samsung QLED55", 3200, "Samsung", 55, true);
         Telewizor Tv2 = new Telewizor("LG OLED65", 4100, "LG", 65, true);
-        Produkt Laptop1 = new Produkt("Lenovo Legion5", 4500, "Lenovo");
-        Produkt Laptop2 = new Produkt("Asus TUF15", 3500, "Asus");
+        Laptop Laptop1 = new Laptop("Lenovo Legion5", 4500, "Lenovo", 16, "Ryzen 7");
+        Laptop Laptop2 = new Laptop("Asus TUF15", 3500, "Asus", 16, "Intel i5");
 
         Tv1.WyswietlInformacje();
         Tv2.WyswietlInformacje();
